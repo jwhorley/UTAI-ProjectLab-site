11cc67a97f22b5f31971c69d37c82071290da432
J <36215234+jwhorley@users.noreply.github.com>
2025-09-16 23:51:09 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -89,7 +89,7 @@ function App() {
         {/* 
         <div className="absolute inset-0 w-full h-full z-0">
           <Spline
-            scene="***SPLINE HERE***"
+            scene="https://prod.spline.design/FgZH78cVMuva2ViP/scene.splinecode"
             onLoad={onSplineLoad}
             onError={onSplineError}
             style={{
@@ -111,10 +111,6 @@ function App() {
         </div>
         */}
 
-        {/* Fallback animated background - shows if Spline doesn't load */}
-        {!splineLoaded && (
-          <div className="absolute inset-0 w-full h-full z-0">
-            <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-orange-500/10 rounded-full blur-3xl animate-pulse"></div>
             <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-amber-500/10 rounded-full blur-3xl animate-pulse delay-1000"></div>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-gradient-to-r from-orange-500/5 to-amber-500/5 rounded-full blur-3xl"></div>
             <div className="absolute inset-0 bg-grid-pattern opacity-20"></div>
