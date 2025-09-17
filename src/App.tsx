14cdf9e34fcb69def4cf36eae7bf895c5360d3ba
J <36215234+jwhorley@users.noreply.github.com>
2025-09-17 00:14:17 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -89,8 +89,6 @@ function App() {
         {/* 
         {/* Spline component temporarily disabled until valid URL is provided */}
         {/* 
-        {/* Spline component temporarily disabled until valid URL is provided */}
-        {/* 
         <div className="absolute inset-0 w-full h-full z-0">
           <Spline
             scene="***SPLINE HERE***"
@@ -114,12 +112,10 @@ function App() {
           />
         </div>
         */}
-        */}
-        */}
 
         {/* Fallback background when Spline is not loaded */}
         {!splineLoaded && (
-          <div className="absolute inset-0">
+          <div className="absolute inset-0 z-0">
             <div className="absolute top-1/4 left-1/4 w-64 h-64 bg-orange-500/10 rounded-full blur-3xl animate-pulse"></div>
             <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-amber-500/10 rounded-full blur-3xl animate-pulse delay-1000"></div>
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[800px] h-[800px] bg-gradient-to-r from-orange-500/5 to-amber-500/5 rounded-full blur-3xl"></div>
